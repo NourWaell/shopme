02d04559ccebd6f260c4449681334b9b6d52b5d7
nour <nourwael1530@gmail.com>
2024-04-18 21:36:59 +0200

    created the functionality to check if the email address in the registration form is available to use and display messages for the availability status for better ux

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,6 +1,7 @@
 import { Input } from "@components/Form";
 import { Heading } from "@components/shared";
 import { zodResolver } from "@hookform/resolvers/zod";
+import useCheckEmailAvailability from "@hooks/useCheckEmailAvailability";
 import { signUpSchema, signUpTypes } from "@validations/signUpSchema";
 import { Button, Col, Form, Row } from "react-bootstrap";
 import { SubmitHandler, useForm } from "react-hook-form";
@@ -9,11 +10,34 @@ const Register = () => {
   const {
     register,
     handleSubmit,
+    getFieldState,
+    trigger,
     formState: { errors },
   } = useForm<signUpTypes>({
     mode: "onBlur",
     resolver: zodResolver(signUpSchema),
   });
+
+  const {
+    checkEmailAvailability,
+    emailAvailabilityStatus,
+    enteredEmail,
+    resetCheckEmailAvailability,
+  } = useCheckEmailAvailability();
+
+  const emailOnBlurHandler = async (e: React.FocusEvent<HTMLInputElement>) => {
+    await trigger("email");
+    const value = e.target.value;
+    const { isDirty, invalid } = getFieldState("email");
+
+    if (isDirty && !invalid && enteredEmail !== value) {
+      checkEmailAvailability(value);
+    }
+
+    if (isDirty && invalid && enteredEmail) {
+      resetCheckEmailAvailability();
+    }
+  };
 
   const submitForm: SubmitHandler<signUpTypes> = (data) => {
     console.log(data);
@@ -26,29 +50,50 @@ const Register = () => {
         <Col md={{ span: 6, offset: 3 }}>
           <Form onSubmit={handleSubmit(submitForm)}>
             <Input
-              label="First name"
+              label="First Name"
               name="firstName"
               register={register}
               error={errors.firstName?.message as string}
             />
 
             <Input
-              label="Last name"
+              label="Last Name"
               name="lastName"
               register={register}
               error={errors.lastName?.message as string}
             />
 
             <Input
-              label="Email address"
+              label="Email Address"
               name="email"
               register={register}
-              error={errors.email?.message as string}
+              onBlur={emailOnBlurHandler}
+              error={
+                errors.email?.message
+                  ? errors.email?.message
+                  : emailAvailabilityStatus === "notAvailable"
+                  ? "This email is already in use."
+                  : emailAvailabilityStatus === "failed"
+                  ? "Error from the server."
+                  : ""
+              }
+              formText={
+                emailAvailabilityStatus === "checking"
+                  ? "We're currently checking the availability of this email address. Please wait a moment."
+                  : ""
+              }
+              success={
+                emailAvailabilityStatus === "available"
+                  ? "This email is available for use."
+                  : ""
+              }
+              disabled={emailAvailabilityStatus === "checking" ? true : false}
             />
 
             <Input
               label="Password"
               name="password"
+              type="password"
               register={register}
               error={errors.password?.message as string}
             />
@@ -56,11 +101,17 @@ const Register = () => {
             <Input
               label="Confirm Password"
               name="confirmPassword"
+              type="password"
               register={register}
               error={errors.confirmPassword?.message as string}
             />
 
-            <Button variant="info" type="submit" style={{ color: "white" }}>
+            <Button
+              variant="info"
+              type="submit"
+              style={{ color: "white" }}
+              disabled={emailAvailabilityStatus === "checking" ? true : false}
+            >
               Submit
             </Button>
           </Form>
--- src/validations/signInSchema.ts
+++ src/validations/signInSchema.ts
@@ -4,10 +4,7 @@ const signInSchema = z.object({
   email: z.string().min(1, { message: "Email address is required" }).email(),
   password: z
     .string()
-    .min(8, { message: "Password must be at least 8 characters longs" })
-    .regex(/.*[!@#$%^&*()_+{}|[\]\\:";'<>?,./].*/, {
-      message: "Password should contain at least 1 special character",
-    }),
+    .min(8, { message: "Password must be at least 8 characters longs" }),
 });
 
 type signInType = z.infer<typeof signInSchema>;

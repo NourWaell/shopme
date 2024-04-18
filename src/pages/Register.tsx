8419fb53ae5015dcef2b33716b252d6bc22c1419
nour <nourwael1530@gmail.com>
2024-04-18 19:23:39 +0200

    created initial ui for registration and login pages

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,4 +1,60 @@
+import { Heading } from "@components/shared";
+import { Button, Col, Form, Row } from "react-bootstrap";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type TFormInputs = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
 const Register = () => {
-  return <div>Register</div>;
+  const { register, handleSubmit } = useForm<TFormInputs>();
+
+  const submitForm: SubmitHandler<TFormInputs> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <Heading title="Register" />
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <Form onSubmit={handleSubmit(submitForm)}>
+            <Form.Group className="mb-3">
+              <Form.Label>First name</Form.Label>
+              <Form.Control type="text" {...register("firstName")} />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Last name</Form.Label>
+              <Form.Control type="text" {...register("lastName")} />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="text" {...register("email")} />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" {...register("password")} />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control type="password" {...register("confirmPassword")} />
+            </Form.Group>
+
+            <Button variant="info" type="submit" style={{ color: "white" }}>
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </>
+  );
 };
 export default Register;

350fa0b1a3235e994cacb1907a8bc55199b1daaf
nour <nourwael1530@gmail.com>
2024-04-18 20:07:43 +0200

    created initial form and validation schema using react hook form and zod

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,19 +1,20 @@
 import { Heading } from "@components/shared";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signUpSchema, signUpTypes } from "@validations/signUpSchema";
 import { Button, Col, Form, Row } from "react-bootstrap";
 import { SubmitHandler, useForm } from "react-hook-form";
 
-type TFormInputs = {
-  firstName: string;
-  lastName: string;
-  email: string;
-  password: string;
-  confirmPassword: string;
-};
-
 const Register = () => {
-  const { register, handleSubmit } = useForm<TFormInputs>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<signUpTypes>({
+    mode: "onBlur",
+    resolver: zodResolver(signUpSchema),
+  });
 
-  const submitForm: SubmitHandler<TFormInputs> = (data) => {
+  const submitForm: SubmitHandler<signUpTypes> = (data) => {
     console.log(data);
   };
 
@@ -25,27 +26,62 @@ const Register = () => {
           <Form onSubmit={handleSubmit(submitForm)}>
             <Form.Group className="mb-3">
               <Form.Label>First name</Form.Label>
-              <Form.Control type="text" {...register("firstName")} />
+              <Form.Control
+                type="text"
+                {...register("firstName")}
+                isInvalid={!!errors.firstName?.message}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.firstName?.message}
+              </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group className="mb-3">
               <Form.Label>Last name</Form.Label>
-              <Form.Control type="text" {...register("lastName")} />
+              <Form.Control
+                type="text"
+                {...register("lastName")}
+                isInvalid={!!errors.lastName?.message}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.lastName?.message}
+              </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group className="mb-3">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="text" {...register("email")} />
+              <Form.Control
+                type="text"
+                {...register("email")}
+                isInvalid={!!errors.email?.message}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.email?.message}
+              </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group className="mb-3">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" {...register("password")} />
+              <Form.Control
+                type="password"
+                {...register("password")}
+                isInvalid={!!errors.password?.message}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.password?.message}
+              </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group className="mb-3">
               <Form.Label>Confirm Password</Form.Label>
-              <Form.Control type="password" {...register("confirmPassword")} />
+              <Form.Control
+                type="password"
+                {...register("confirmPassword")}
+                isInvalid={!!errors.confirmPassword?.message}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.confirmPassword?.message}
+              </Form.Control.Feedback>
             </Form.Group>
 
             <Button variant="info" type="submit" style={{ color: "white" }}>
